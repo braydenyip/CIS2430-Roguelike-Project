f982503cbba5b5e2b5b9d15b0915bcea2b5cfb44
Brayden Yip <yipb@uoguelph.ca>
2020-11-27 23:10:45 -0500

    added a Potion subclass of Magic, that is consumable and tossable

---
+++ src/main/java/rogue/Potion.java
@@ -0,0 +1,51 @@
+package rogue;
+
+import java.util.Random;
+
+public class Potion extends Magic implements Consumable, Tossable {
+
+  private Random random = new Random();
+  private int potency;
+
+  /**
+  * The default constructor.
+  */
+  public Potion() {
+    super();
+    setPotency(random.nextInt(101));
+  }
+
+  /**
+  * Returns a strength value of the potion.
+  * @return (int) the potency of the potion.
+  */
+  public int getPotency() {
+    return potency;
+  }
+
+  /**
+  * Sets the potency value of the potion.
+  * @param newPotency the potency value for the potion
+  */
+  public void setPotency(int newPotency) {
+    potency = newPotency;
+  }
+  
+  /**
+  * Generates a message for when you drinl the potion.
+  * @return (String) the message to print when the potion is consumed.
+  */
+  @Override
+  public String consume() {
+    return ("You drink the " + getName());
+  }
+
+  /**
+  * Generates a message for when you throw the potion.
+  * @return (String) the message to print when the potion is thrown
+  */
+  @Override
+  public String toss() {
+    return ("You throw the" + getName() + " and it breaks.");
+  }
+}
