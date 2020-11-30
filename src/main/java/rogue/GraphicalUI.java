0f32daba9c40e013e70790ff243443bb4ed679f0
Brayden Yip <yipb@uoguelph.ca>
2020-11-30 18:39:34 -0500

    Added a basic game window

--- src/main/java/rogue/GraphicalUI.java
+++ src/main/java/rogue/GraphicalUI.java
@@ -1,61 +1,77 @@
 package rogue;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+//import javax.swing.JTextField;
+//import javax.swing.JTextArea;
+
+//import java.awt.FlowLayout;
+import java.awt.GridBagLayout;
+import java.awt.Container;
 
 /**
 * The GraphicalUI class makes a JFrame that contains the entire game UI.
 */
 public class GraphicalUI extends JFrame {
 
-  public static final int WIDTH = 800;
+  public static final int WIDTH = 1000;
   public static final int HEIGHT = 700;
+  private Container contentPane;
 
   /**
   * Default GUI constructor.
   */
   public GraphicalUI() {
     super();
+    setDefaults();
+    setLabels();
   }
 
+  /**
+  * Sets the basic parameters of the game window.
+  */
+  public void setDefaults() {
+    setSize(WIDTH, HEIGHT);
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    contentPane = getContentPane();
+    contentPane.setLayout(new GridBagLayout());
+  }
+
+  /**
+  * Sets the labels.
+  */
+  public void setLabels() {
+    JPanel messagePanel = new JPanel();
+    JPanel instructPanel = new JPanel();
+    messagePanel.add(new JLabel("This is a test message"));
+    instructPanel.add(new JLabel("Press the button in the upper-right corner to exit."));
+    contentPane.add(messagePanel);
+    contentPane.add(instructPanel);
+  }
+
+  /**
+  * Sets the statistical fields.
+  * @param playerName the name of the player
+  */
+  public void setStatsPanel(String playerName) {
+    JPanel statsPanel = new JPanel();
+    JLabel playerNameLabel = new JLabel(playerName);
+    statsPanel.add(playerNameLabel);
+    contentPane.add(statsPanel);
+  }
+  /**
+  * The main executable.
+  * @param args this program does not use any command line arguments.
+  */
   public static void main(String[] args) {
-    System.out.println("---");
     GraphicalUI gui = new GraphicalUI();
     char userInput = 'h';
     String message;
     String configurationFileLocation = "fileLocations.json";
     //Parse the json files
     RogueParser parser = new RogueParser(configurationFileLocation);
-    //allocate memory for the GUI
-    TextUI theGameUI = new TextUI();
-    // allocate memory for the game and set it up
-    Rogue theGame = new Rogue(parser);
-   //set up the initial game display
-    Player thePlayer = new Player("Brayden");
-    theGame.setPlayer(thePlayer);
-    theGame.initializeGameState();
-
-    if (theGame.verifyAllRooms()) {
-      message = "Welcome to my Rogue game";
-      theGameUI.draw(message, theGame.getNextDisplay());
-    } else {
-      message = "The rooms file could not be used.\n";
-      theGameUI.draw(message, "Press 'q' to quit\n");
-    }
-
-    while (userInput != 'q') {
-      //get input from the user
-      userInput = theGameUI.getInput();
-
-      //ask the game if the user can move there
-      try {
-        message = theGame.makeMove(userInput);
-        theGameUI.draw(message, theGame.getNextDisplay());
-      } catch (InvalidMoveException badMove) {
-          theGameUI.setMessage(badMove.getMessage());
-      }
-    }
-    // do something here to say goodbye to the user
-    System.out.println("Thanks for playing!");
+    gui.setStatsPanel("Brayden");
+    gui.setVisible(true);
   }
 }
