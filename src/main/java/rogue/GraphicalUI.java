766cd601ba19b0770424b984ed45efec6bd03374
Brayden Yip <yipb@uoguelph.ca>
2020-11-30 15:29:35 -0500

    copy pasted Main from textUI into the GraphicalUI

--- src/main/java/rogue/GraphicalUI.java
+++ src/main/java/rogue/GraphicalUI.java
@@ -21,5 +21,41 @@ public class GraphicalUI extends JFrame {
   public static void main(String[] args) {
     System.out.println("---");
     GraphicalUI gui = new GraphicalUI();
+    char userInput = 'h';
+    String message;
+    String configurationFileLocation = "fileLocations.json";
+    //Parse the json files
+    RogueParser parser = new RogueParser(configurationFileLocation);
+    //allocate memory for the GUI
+    TextUI theGameUI = new TextUI();
+    // allocate memory for the game and set it up
+    Rogue theGame = new Rogue(parser);
+   //set up the initial game display
+    Player thePlayer = new Player("Brayden");
+    theGame.setPlayer(thePlayer);
+    theGame.initializeGameState();
+
+    if (theGame.verifyAllRooms()) {
+      message = "Welcome to my Rogue game";
+      theGameUI.draw(message, theGame.getNextDisplay());
+    } else {
+      message = "The rooms file could not be used.\n";
+      theGameUI.draw(message, "Press 'q' to quit\n");
+    }
+
+    while (userInput != 'q') {
+      //get input from the user
+      userInput = theGameUI.getInput();
+
+      //ask the game if the user can move there
+      try {
+        message = theGame.makeMove(userInput);
+        theGameUI.draw(message, theGame.getNextDisplay());
+      } catch (InvalidMoveException badMove) {
+          theGameUI.setMessage(badMove.getMessage());
+      }
+    }
+    // do something here to say goodbye to the user
+    System.out.println("Thanks for playing!");
   }
 }
