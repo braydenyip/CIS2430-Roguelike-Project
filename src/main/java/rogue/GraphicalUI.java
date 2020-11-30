6f625b65421636305399504fd5c5186de9bbb777
Brayden Yip <yipb@uoguelph.ca>
2020-11-30 02:32:15 -0500

    Add Javadoc comments to GraphicalUI

--- src/main/java/rogue/GraphicalUI.java
+++ src/main/java/rogue/GraphicalUI.java
@@ -3,11 +3,23 @@ package rogue;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+* The GraphicalUI class makes a JFrame that contains the entire game UI.
+*/
 public class GraphicalUI extends JFrame {
 
+  public static final int WIDTH = 800;
+  public static final int HEIGHT = 700;
+
+  /**
+  * Default GUI constructor.
+  */
   public GraphicalUI() {
     super();
   }
 
-  
+  public static void main(String[] args) {
+    System.out.println("---");
+    GraphicalUI gui = new GraphicalUI();
+  }
 }
